6ee2d9b1b3b9b90ee32f7a1b1b139bd589cae051
CosmicOrder <Matrixxturn@yandex.ru>
2023-03-04 16:50:33 +0300

    refactor: move config from code

--- redis_connection.py
+++ redis_connection.py
@@ -4,12 +4,14 @@ from environs import Env
 env = Env()
 env.read_env()
 
+host = env('HOST')
+port = env('PORT')
 username = env('REDIS_USERNAME')
 password = env('REDIS_PASSWD')
 
 redis = redis.Redis(
-    host='redis-11457.c1.asia-northeast1-1.gce.cloud.redislabs.com',
-    port=11457,
+    host=host,
+    port=port,
     username=username,
     password=password,
     decode_responses=True,
